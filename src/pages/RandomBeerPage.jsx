7de206b8502cca84e5e57956553b6f5ab2545297
lisifun <lisandrafj99@gmail.com>
2023-12-24 15:29:24 -0700

    adding fourth iteration

--- src/pages/RandomBeerPage.jsx
+++ src/pages/RandomBeerPage.jsx
@@ -1,23 +1,30 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import beersJSON from "./../assets/beers.json";
+import axios from "axios";
 
+import { API_URL } from "../services/API_URL";
 
 function RandomBeersPage() {
   // Mock initial state, to be replaced by data from the Beers API. Store the beer info retrieved from the Beers API in this state variable.
-  const [randomBeer, setRandomBeer] = useState(beersJSON[0]);
+  const [randomBeer, setRandomBeer] = useState(null);
 
   // React Router hook for navigation. We use it for the back button. You can leave this as it is.
   const navigate = useNavigate();
 
-
-  
+  console.log("this is the random beer => ", randomBeer);
   // TASKS:
   // 1. Set up an effect hook to make a request for a random beer from the Beers API.
   // 2. Use axios to make a HTTP request.
   // 3. Use the response data from the Beers API to update the state variable.
-
-
+  useEffect(() => {
+    axios
+      .get(`${API_URL}beers/random`)
+      .then((response) => {
+        setRandomBeer(response.data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
 
   // The logic and the structure for the page showing the random beer. You can leave this as it is.
   return (
